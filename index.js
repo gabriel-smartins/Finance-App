33b9ccc4b8ed7ce04abafe065ee7e6be7505a078
Gabriel Martins <gab.smartinn@outlook.com>
2025-07-10 15:40:09 -0300

    feat add create user controller and validations

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import 'dotenv/config.js'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/api/users' , async (request , response) => {
+    const createUserController = new CreateUserController()
+
+    const createUserResponse = await createUserController.execute(request)
+
+    response.status(createUserResponse.statusCode).send(createUserResponse.body)
+})
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,30 +1,29 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
-import { PostgresCreateUserRepository } from '../repositories/postgres/create-user'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 
-export class CreateUserService {
+export class CreateUserUseCase {
     async execute(createUserParams) {
         //TO DO: verificar se o email já está em uso
-
 
         // gerar ID do usuário
         const userId = uuidv4()
 
         //criptografar senha
-        const hashPassword = await bcrypt.hash(createUserParams.password , 10)
+        const hashPassword = await bcrypt.hash(createUserParams.password, 10)
 
         //inserir usuário no banco
         const user = {
-          ... createUserParams,
-          id: userId,
-          password: hashPassword,
+            ...createUserParams,
+            id: userId,
+            password: hashPassword,
         }
 
         // chamar repositorio
-        const postgresCreateUserRepository = new PostgresCreateUserRepository
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
         const createdUser = postgresCreateUserRepository.execute(user)
 
-        return createdUser  
+        return createdUser
     }
 }
